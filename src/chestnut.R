0ffe7f5806de9e3f0b6b4133405f8786a03f6844
mklau <matthewklau@fas.harvard.edu>
2016-10-12 16:48:53 -0400

    organized code, ends with a plot

--- src/chestnut.R
+++ src/chestnut.R
@@ -1,29 +1,35 @@
-library(ggplot2)
+### Analysis of chestnut data from the HF megaplot
+### MKLau 12October2016
 
+### Check/install/library dependencies
+packs <- c('ggplot2')
+sapply(packs,function(x) if (!(x %in% installed.packages()[,1])){install.packages(x)})
+sapply(packs,require,quiet=T,char=T)
+
+### Import data from HF archives HF253
 elev <- read.csv('http://harvardforest.fas.harvard.edu/data/p25/hf253/hf253-01-elevation.csv')
 codes <- read.csv('http://harvardforest.fas.harvard.edu/data/p25/hf253/hf253-02-species-codes.csv')
 trees <- read.csv('http://harvardforest.fas.harvard.edu/data/p25/hf253/hf253-03-trees-2014.csv')
 stems <- read.csv('http://harvardforest.fas.harvard.edu/data/p25/hf253/hf253-04-stems-2014.csv')
 
-codes[agrep('castanea',codes[,'latin'],ign=T),]
+### get the code for chestnut
+codes[agrep('castanea',codes[,'latin'],ign=T),'sp']
 
+### delimit the dataset to chestnut, merging trees and stems
 cade.tr <- trees[trees[,'sp'] == 'castde',]
 cade.st <- stems[stems[,'sp'] == 'castde',]
 cade <- rbind(cade.tr[,colnames(cade.tr) %in% colnames(cade.st)],cade.st[,colnames(cade.st) %in% colnames(cade.tr)])
 cade <- cade[duplicated(cade[,'tree.id']),]
 
-head(cade)
+### counts of chestnut
 table(cade[,'df.status'])
 
-cade.plot <- ggplot(data=cade,aes(x=gx,y=gy,color=df.status)) + geom_point() 
-contour <- ggplot(data=elev,aes(x=col.x,y=col.y,z=col.elev)) + geom_contour(color='darkgrey')
-
-contour + geom_point(data=cade,aes(x=gx,y=gy,color=df.status))
-
+### chestnut map with elevation contours
 ggplot() +
     stat_contour(data = elev, aes(x = col.x, y = col.y, z = col.elev, color = ..level..)) + 
         labs(color = "density") + 
-            geom_point(data = cade, aes(x = gx, y = gy, fill = factor(df.status)), pch = 21) + 
-                scale_color_continuous(guide=guide_legend(title='Elevation')) + 
-                    scale_fill_manual(guide=guide_legend(title='Status'),values=c('lightgreen','darkred')) 
+            geom_point(data = cade, aes(x = gx, y = gy, fill = factor(df.status), size=dbh), pch = 21) + 
+                scale_fill_manual(guide=guide_legend(title=''),values=c('darkgreen','darkred')) + 
+                    scale_color_continuous(guide=guide_legend(title='Elevation')) + 
+                        xlab('x') + ylab('y')
 
