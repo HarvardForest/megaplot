004de75c4662fe49df20d67f934f5e3dcf27f1c9
mklau <matthewklau@fas.harvard.edu>
2016-11-28 15:00:19 -0500

    adds more CaDe analyses

--- src/sppMap.R
+++ src/sppMap.R
@@ -35,3 +35,23 @@ ggplot() +
                     scale_color_continuous(guide=guide_legend(title='Elevation')) + 
                         xlab('x') + ylab('y')
 
+
+plot(density(cade[cade$df.status == 'alive','dbh']),col='green',main='',xlim=range(cade[,'dbh']) * c(0,1))
+lines(density(cade[cade$df.status == 'dead','dbh']),col='red',main='')
+
+
+d.cade <- dist(cade[,c('gx','gy')])
+m.cade <- as.matrix(d.cade)
+mc.col <- as.numeric(cade[,'df.status'])
+mc.col <- mc.col %*% t(mc.col)
+diag(mc.col) <- 0
+image(m.cade,col=mc.col)
+
+ut <- m.cade[upper.tri(mc.col)]
+ut.col <- mc.col[upper.tri(mc.col)]
+
+plot(ut~ut.col)
+
+plot(density(ut[ut.col == 1]),col='green',xlim=range(ut),main='',ylim=c(0,0.0035))
+lines(density(ut[ut.col == 2]),col='red')
+lines(density(ut[ut.col == 4]),col='black')
